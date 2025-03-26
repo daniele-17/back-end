ebeddee16f68a4ffaf24f9788cacb92bf3dc17d8
dani <antunes.cristina.daniele@gmail.com>
2025-03-26 13:54:06 +0000

    Aula 01 variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+
+    public static void main(String[] args) {
+        // Varíavel logica
+
+        // Tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+
+        // Tipo abstrato
+        boolean valorLogicoAbstrato = false;
+    }
+}
