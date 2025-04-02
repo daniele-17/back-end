3652c5dbccbf63558cf739760682ca30dcd17a3b
dani <antunes.cristina.daniele@gmail.com>
2025-04-02 19:40:19 +0000

    Aula03Operadores

---
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -0,0 +1,18 @@
+package com.mycompany;
+
+public class Aula03Operadores {
+    public static void main(String[] args) {
+        /**
+         * Tipos de Comentários
+         */
+
+        // Comentários de uma linha
+
+        /**
+         * Comentários de 
+         * múltiplas
+         * linhas
+         */
+        
+    }
+}
