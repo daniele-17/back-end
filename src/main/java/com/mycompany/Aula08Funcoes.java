a9b28eb851ef04b9f74644c156428a612503fc8f
dani <antunes.cristina.daniele@gmail.com>
2025-06-04 19:33:13 +0000

    Aula 08 funções recursivas

--- src/main/java/com/mycompany/Aula08Funcoes.java
+++ src/main/java/com/mycompany/Aula08Funcoes.java
@@ -1,19 +1,43 @@
-oublic class Aula08Funcoes {
-Run | Debug
-public static void main(String[] args) {
-int numero = 5;
+    package com.mycompany;
 
-System.out.println(" --- Tabuada do "
+    public class Aula08Funcoes
+    public static void main(String[] args) {
+    int numero = 5;
 
-for (int i = 1; i <= 10; ++) {
-System.out.println(numero + "
+    System.out.println(" --- Tabuada do " + numero
 
-X
+    for (int i = 1; i <= 10; i++) {
+    System.out.println(numero + " x" + i + " = " + multiplicacao(numero, i);
+    }
 
-+
+    System.out.println(fatorial(n:5))
+ }
 
-+ multiplicacao(numero,
 
-public static int multiplicacao(int num, int num2) {
-return num * num2;
+    /**
+    * Não posso declarar méto
+    *
+    Toda funcao deve ter um retorno, quando nao ha retorno utilizamos void.
+    *
+    Uma funcao pode nao ter parametros, ou ter 1 ou mais.
+    */
+    public static int multiplicacao(int num, int num2) {
+    return num * num2;
+    }
 
+    /**
+     * Funcao recursivas
+     * 
+     * uma funcao recursiva faz uma chanmada para ela propria
+     * 
+     * porém, toda recursividade deve ter um ponto de parada,
+     * caso contrario pode resultar em um loop infinito
+     */
+    public static int fatorial(int n) {
+        if (n != 0) {
+            return n * fatorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+}
